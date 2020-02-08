d36383be60fe61a44433f99a48fa820593c1ce55
Colton <colton@Sulaco.domain>
2020-02-08 16:43:41 -0700

    initial files

---
+++ craps.js
@@ -0,0 +1,18 @@
+var CRAPS = {};
+
+(function($) {
+
+  CRAPS = {
+
+    home: "home is the best value ever",
+
+    init: function() {
+      // $('#title').text(this.home);
+      // console.log(this.home);
+    }
+
+  };
+
+})(jQuery);
+
+CRAPS.init();
